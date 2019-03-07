a7eeb1f14c3e45c3991b1637b23272ff4f14e0e2
Jake Hershberger <jake.hershberger@primegov.com>
2019-03-07 11:16:35 -0700

    added goodyear forms

--- routes/goodyear.js
+++ routes/goodyear.js
@@ -13,4 +13,12 @@ module.exports = function (app) {
   app.get(innerUrl + 'Staff_Report_New', function (req, res) {
     res.render("goodyear/Staff_Report_New", {clientUrl: clientUrl,client: client,form: false, innerUrl: false});
   });
+  app.get(innerUrl + 'Staff_Report_P&Z_NEW', function (req, res) {
+    res.render("goodyear/Staff_Report_P&Z_NEW", {clientUrl: clientUrl,client: client,form: 'Staff_Report_P&Z_NEW', innerUrl: false});
+  });
+  //Communication_Items
+  app.get(innerUrl + 'Communication_Items', function (req, res) {
+    res.render("goodyear/Communication_Items", {clientUrl: clientUrl,client: client,form: 'Communication Items', innerUrl: false});
+  });
+
 }
