b1ab37593053a571e631c8e5d2fd02b2be968ad9
Jake Hershberger <jake.hershberger@primegov.com>
2019-03-17 20:36:52 -0600

    update

--- routes/goodyear.js
+++ routes/goodyear.js
@@ -8,17 +8,23 @@ var innerUrl = "/goodyear/"
 module.exports = function (app) {
 
   app.get(innerUrl, function (req, res) {
-    res.render("goodyear/index", {clientUrl: clientUrl,client: client,form: false, innerUrl: false});
+    res.render("goodyear/index", { clientUrl: clientUrl, client: client, form: false, innerUrl: false });
   });
-  app.get(innerUrl + 'Staff_Report_New', function (req, res) {
-    res.render("goodyear/Staff_Report_New", {clientUrl: clientUrl,client: client,form: false, innerUrl: false});
+  //City Council Action Request (CAR)
+  app.get(innerUrl + 'City_Council_Action_request', function (req, res) {
+    res.render("goodyear/City_Council_Action_request", { clientUrl: clientUrl, client: client, form: "City Council Action Request (CAR)", innerUrl: false });
   });
-  app.get(innerUrl + 'Staff_Report_P&Z_NEW', function (req, res) {
-    res.render("goodyear/Staff_Report_P&Z_NEW", {clientUrl: clientUrl,client: client,form: 'Staff_Report_P&Z_NEW', innerUrl: false});
+  //P&Z City Council Action Request (CAR)
+  app.get(innerUrl + 'P&ZCity_Council_Action_request', function (req, res) {
+    res.render("goodyear/P&ZCity_Council_Action_request", { clientUrl: clientUrl, client: client, form: 'P&Z City Council Action Request (CAR)', innerUrl: false });
   });
-  //Communication_Items
-  app.get(innerUrl + 'Communication_Items', function (req, res) {
-    res.render("goodyear/Communication_Items", {clientUrl: clientUrl,client: client,form: 'Communication Items', innerUrl: false});
+  //Communication City Council Action Request (CAR)	
+  app.get(innerUrl + 'Communication_City_Council_Action_request', function (req, res) {
+    res.render("goodyear/Communication_City_Council_Action_request", { clientUrl: clientUrl, client: client, form: 'Communication City Council Action Request (CAR)	', innerUrl: false });
+  });
+  //Community Facilities District (CFD) City Council Action Request (CAR)
+  app.get(innerUrl + 'CommunityFacilitiesDistrict', function (req, res) {
+    res.render("goodyear/CommunityFacilitiesDistrict", { clientUrl: clientUrl, client: client, form: 'Community Facilities District (CFD) ', innerUrl: false });
   });
 
 }
